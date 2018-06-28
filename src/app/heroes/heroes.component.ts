71e3ff847c9f866fb09cf06dd0a88e0994a57365
Shivam <sihare123@gmail.com>
2018-06-28 10:48:27 +0530

    hero delete

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -27,4 +27,8 @@ export class HeroesComponent implements OnInit {
     this.heroService.addHero({name} as Hero)
       .subscribe((hero) => this.heroes.push(hero));
   }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h.id !== hero.id);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 }
